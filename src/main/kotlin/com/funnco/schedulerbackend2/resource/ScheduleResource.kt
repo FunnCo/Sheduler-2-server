e2da18743b6ded862b24fe3118b335b18c3aa903
Макс <@>
2026-03-01 18:51:20 +0300

    Добавлена ручка для добавления расписания на N недель

--- src/main/kotlin/com/funnco/schedulerbackend2/resource/ScheduleResource.kt
+++ src/main/kotlin/com/funnco/schedulerbackend2/resource/ScheduleResource.kt
@@ -99,8 +99,13 @@ class ScheduleResource {
     }
 
     @PostMapping("/force/create_new")
-    fun forceUpdateNew(){
+    fun forceCreateNew(){
         currentEventsService.forceCreateFutureEventsByTemplate(4)
+    }
+
+    @PostMapping("/force/create_new")
+    fun forceCreateNewForNWeeks(weeks: Int){
+        currentEventsService.forceCreateFutureEventsByTemplate(count)
     }
 
     @DeleteMapping("/force/delete_future")
